9a9f1e0ab885929cca5549817c2fdb36f07017a0
bella1096 <isabellaamr17@gmail.com>
2025-08-10 16:46:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.querySelectorAll(".ramo").forEach(boton => {
-    boton.addEventListener("click", () => {
-        if (boton.classList.contains("bloqueado")) return;
 
-        boton.style.backgroundColor = "#9E4D57";
-
-        let desbloquea = boton.getAttribute("data-desbloquea");
-        if (desbloquea) {
-            desbloquea.split(",").forEach(id => {
-                let ramoDesbloquear = document.getElementById(id.trim());
-                if (ramoDesbloquear) {
-                    ramoDesbloquear.classList.remove("bloqueado");
-                }
-            });
-        }
-    });
-});
