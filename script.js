0f12df656883937bca15eb7629a8793aaf624e8e
bella1096 <isabellaamr17@gmail.com>
2025-08-10 16:42:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.classList.add("aprobado");
+        btn.disabled = true;
 
+        let desbloqueos = btn.dataset.unlock;
+        if (desbloqueos) {
+            desbloqueos.split(",").forEach(id => {
+                let requisito = document.getElementById(id);
+                if (requisito) {
+                    requisito.disabled = false;
+                }
+            });
+        }
+    });
+});
